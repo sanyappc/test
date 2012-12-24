6c0cd7720f5f442c95d5b423ef18b8b4b9fbd492
dvdalex <dvdalex@nm.ru>
2012-12-25 01:18:58 +0400

    make fix

--- make.sh
+++ make.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 LOG = /usr/lib/cgi-bin/test_log
 cp -T test.py /usr/lib/cgi-bin/test
-if [ ! -d $(LOG) ] ; then mkdir $(LOG) ; fi
-chmod 777 $(LOG)
+if [ ! -d $LOG ] ; then mkdir $LOG ; fi
+chmod 777 $LOG
 cp -r test_questions /usr/lib/cgi-bin
 cp -r test_images /var/www
 
