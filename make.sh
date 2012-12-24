d034c37cb8b800a06b2ffd1aafd1ed2db27a47d0
dvdalex <dvdalex@nm.ru>
2012-12-25 00:57:23 +0400

    logging fix

--- make.sh
+++ make.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cp -T test.py /usr/lib/cgi-bin/test
+if [ ! -d /usr/lib/cgi-bin/test_log ] ; then mkdir /usr/lib/cgi-bin/test_log ; fi
 cp -r test_questions /usr/lib/cgi-bin
 cp -r test_images /var/www
 
