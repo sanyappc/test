b15bce9480ecd25eff5fdf0e092809bc2bc91b0d
dvdalex <dvdalex@nm.ru>
2012-12-25 01:25:05 +0400

    long strings fix

--- test.py
+++ test.py
@@ -7,19 +7,21 @@ from os.path import exists,getsize
 import cgi
 import cgitb
 import datetime
-#import logging
-#cgitb.enable(display=0, logdir="/var/log/cgi")
+
 qdir = './test_questions/'
 ldir = './test_log/'
 lfile = ldir+'checks.log'
+
 def getexistence(n):
 	return exists(qdir+str(n)+'.txt')
 def getanswer(n,i):
-	f = filter(lambda x: '|' in x,open(qdir+str(n)+'.txt','r').read().split('\n'))[i].split('|')
+	f = filter(lambda x: '|' in x,open(qdir+str(n)+'.txt','r').
+		read().split('\n'))[i].split('|')
 	return int(f[len(f)-1])
 def getquestion(n):
 	random.seed()
-	f = filter (lambda x: '|' in x,open(qdir+str(n)+'.txt','r').read().split('\n'))
+	f = filter (lambda x: '|' in x,open(qdir+str(n)+'.txt','r').
+		read().split('\n'))
 	i = random.randrange(len(f))
 	return f[i].split('|')[:-1],i
 		
@@ -46,9 +48,12 @@ if form.getvalue('submit'):
 			if getanswer(n,int(ans[0])) == int(ans[1]):
 				k+=1
 		n+=1
-	print "\t\t<h1> Result is "+str(k)+"/"+str(n-1)+" &#8212; "+str(100*k/float(n-1))+"% </h1>"
+	print "\t\t<h1> Result is "+str(k)+"/"+str(n-1)+" &#8212; "+\
+		str(100*k/float(n-1))+"% </h1>"
 	#let's to log without logging and without rotation
-	log = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')+' surname: '+form.getvalue('surname')+' result: '+str(k)+'/'+str(c)+'/'+str(n-1)+' - '+str(100*k/float(n-1))+'%\n'
+	log = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')+\
+		' surname: '+form.getvalue('surname')+' result: '+str(k)+\
+		'/'+str(c)+'/'+str(n-1)+' - '+str(100*k/float(n-1))+'%\n'
 	if exists(lfile) and (getsize(lfile) < 1024 * 1024):
 		open(lfile,'a+').write(log)
 	else:
@@ -61,11 +66,14 @@ else:
 		print "\t\t\t<p>"
 		print "\t\t\t\t"+str(n)+". "+f[0]+" <br/>"
 		for t in range(1,len(f)):
-			print '\t\t\t\t<input type="radio" name="'+str(n)+'" value="'+str(i)+';'+str(t)+'"/> '+f[t]+" <br/>"
+			print '\t\t\t\t<input type="radio" name="'+str(n)+\
+				'" value="'+str(i)+';'+str(t)+'"/> '+f[t]+" <br/>"
 		print "\t\t\t</p>"
 		n+=1
 	print "\t\t\t<p>"
 	print '\t\t\t\tsurname: <input type="text" name="surname" value=""/> <br/>'
+	print "\t\t\t</p>"
+	print "\t\t\t<p>"
 	print '\t\t\t\t<input type="submit" name="submit" value="check!"/>'
 	print "\t\t\t</p>"
 	print '\t\t</form>'
