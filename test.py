4e499ee3c6fdc305b8e2796a7aba9b23d08f0ed6
dvdalex <dvdalex@nm.ru>
2012-12-26 21:47:16 +0400

    hash fix

--- test.py
+++ test.py
@@ -24,6 +24,8 @@ def getquestion(n):
 		read().split('\n'))
 	i = random.randrange(len(f))
 	return f[i].split('|')[:-1],i
+def gethash(i):
+	return str(hash(('fefsfrrgref','hthteeees')+i+('awfcngmhfbb','ajwbfucwwa')))
 		
 cgitb.enable(display=0, logdir=ldir)
 form = cgi.FieldStorage()
@@ -38,35 +40,46 @@ print "\t\t<title>Test</title>"
 print "\t</head>"
 print "\t<body>"
 if form.getvalue('submit'):
-	k = 0 # true
-	c = 0 # checked
-	n = 1
-	while getexistence(n):
-		if form.getvalue(str(n)):
-			ans = form.getvalue(str(n)).split(';')
-			c += 1
-			if getanswer(n,int(ans[0])) == int(ans[1]):
-				k+=1
-		n+=1
-	print "\t\t<h1> Result is "+str(k)+"/"+str(n-1)+" &#8212; "+\
-		str(100*k/float(n-1))+"% </h1>"
-	#let's log without logging and without rotation
 	if form.getvalue('surname'):
-		surname = form.getvalue('surname')
-	else:
-		surname = 'not specified'
-	log = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')+\
-		' surname: '+surname+': result: '+str(k)+\
-		'/'+str(c)+'/'+str(n-1)+' - '+str(100*k/float(n-1))+'%\n'
-	if exists(lfile) and (getsize(lfile) < 1024 * 1024):
-		open(lfile,'a+').write(log)
-	else:
-		open(lfile,'w+').write(log)
+		k = 0 # true
+		c = 0 # checked
+		n = 1
+		hashtuple = ()
+		while getexistence(n):
+			if form.getvalue(str(n)):
+				ans = form.getvalue(str(n)).split(';')
+				c += 1
+				hashtuple += (int(ans[0]),)
+				if getanswer(n,int(ans[0])) == int(ans[1]):
+					k+=1
+			else:
+				break
+			n+=1
+		if c == (n-1):
+			if form.getvalue('hidden') == gethash(hashtuple):
+				print "\t\t<h1> Result is "+str(k)+"/"+str(n-1)+" &#8212; "+\
+					str(100*k/float(n-1))+"% </h1>"
+				surname = form.getvalue('surname')
+				#let's log without logging and without rotation
+				log = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')+\
+					' surname: '+surname+': result: '+str(k)+\
+					'/'+str(n-1)+' - '+str(100*k/float(n-1))+'%\n'
+				if exists(lfile) and (getsize(lfile) < 1024 * 1024):
+					open(lfile,'a+').write(log)
+				else:
+					open(lfile,'w+').write(log)
+			else:
+				print "\t\t<h1> Wrong data </h1>"
+		else:
+			print "\t\t<h1> Answer to all questions is required </h1>"
+	else: print "\t\t<h1> Surname is required </h1>"
 else:
 	n = 1
+	hashtuple = ()
 	print '\t\t<form action="/cgi-bin/test" method="POST">'
 	while getexistence(n):
 		f,i = getquestion(n)
+		hashtuple += (i,)
 		print "\t\t\t<p>"
 		print "\t\t\t\t"+str(n)+". "+f[0]+" <br/>"
 		for t in range(1,len(f)):
@@ -75,9 +88,10 @@ else:
 		print "\t\t\t</p>"
 		n+=1
 	print "\t\t\t<p>"
-	print '\t\t\t\tsurname: <input type="text" name="surname" value=""/> <br/>'
+	print '\t\t\t\tsurname: <input type="text" name="surname" value=""/>'
 	print "\t\t\t</p>"
 	print "\t\t\t<p>"
+	print '\t\t\t\t<input type="hidden" name="hidden" value="'+gethash(hashtuple)+'">'
 	print '\t\t\t\t<input type="submit" name="submit" value="check!"/>'
 	print "\t\t\t</p>"
 	print '\t\t</form>'
